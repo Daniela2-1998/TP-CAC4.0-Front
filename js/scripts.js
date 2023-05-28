77e9bf8f832dfd0901593921d3dbc1b042f15512
Daniela2-1998 <danimansilla27@gmail.com>
2023-05-28 13:01:48 -0300

    js prepared to calculate prices

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function valorCategoria(){
+    let categoria = document.getElementById("categoria").value;
+    console.log("Se eligió la categoría " + categoria);
+    let descuento;
+    if(categoria == 1){
+        return descuento = 0.20;
+    } else if(categoria == 2){
+        return descuento = 0.50;
+    } else if(categoria == 3){
+        return descuento = 0.85;
+    }
+    console.log("Porcentaje a pagar: " + descuento);
+}
+
+function calcularValorTicket(){
+    const precioBase = 200;
+    let cantidad = document.getElementById("cantidad").value;
+    let porcentajeAPagar = valorCategoria("categoria");
+    let valorTickets = cantidad * precioBase * porcentajeAPagar;
+    console.log("Los tickets solicitados valen: " + valorTickets);
+    let valorTotal = document.getElementById("total").innerHTML = "Total a Pagar: $ " + valorTickets;
+}
+
+
+
